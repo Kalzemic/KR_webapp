7b08633402c6a3abcf61ea4f8f71d6ea3069136b
Kalzemic <Mizizov@gmail.com>
2025-08-24 12:10:07 +0300

    update

--- src/components/ContactPage.tsx
+++ src/components/ContactPage.tsx
@@ -9,26 +9,30 @@ const ContactPage = () => {
     const [phone, setPhone] = useState("")
     const [email, setEmail] = useState("")
     return (
-        <form className='contact-form' >
-
-            <div className='form-segment'>
-                <div className='form-input'>
-                    <label htmlFor='shem' > שם מלא:</label>
-                    <input id='shem' type="text" value={name} onChange={(e) => { setName(e.target.value) }} />
-                </div>
+        <div className='contact-page'>
+            <div className='contact-header'>
+                <h1>צרו קשר </h1>
             </div>
-            <div className='form-segment'>
-                <div className='form-input'>
-                    <label htmlFor='phone' >טלפון:</label>
-                    <input id='phone' type='text' value={phone} onChange={(e) => { setPhone(e.target.value) }} />
+            <form className='contact-form' >
+                <div className='form-segment'>
+                    <div className='form-input'>
+                        <label htmlFor='shem' > שם מלא:</label>
+                        <input id='shem' type="text" value={name} onChange={(e) => { setName(e.target.value) }} />
+                    </div>
                 </div>
-                <div className='form-input'>
-                    <label htmlFor='email' > דוא"ל:</label>
-                    <input id='email' type='text' value={email} onChange={(e) => { setEmail(e.target.value) }} />
+                <div className='form-segment'>
+                    <div className='form-input'>
+                        <label htmlFor='phone' >טלפון:</label>
+                        <input id='phone' type='text' value={phone} onChange={(e) => { setPhone(e.target.value) }} />
+                    </div>
+                    <div className='form-input'>
+                        <label htmlFor='email' > דוא"ל:</label>
+                        <input id='email' type='text' value={email} onChange={(e) => { setEmail(e.target.value) }} />
+                    </div>
                 </div>
-            </div>
-            <button type='submit' >שלח </button>
-        </form>
+                <button type='submit' >שלח </button>
+            </form>
+        </div>
     );
 }
 
--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -1,10 +1,24 @@
 import '../styles/Portfolio.css'
 
 
-const projects = ['project1.png', 'project2.png', 'project3.png', 'project4.png', 'project5.png', 'project6.png']
+const projects = ['project1.png', 'project2.png', 'project3.png', 'project4.png', 'project5.png', 'project6.png', 'project7.png', 'project8.png']
 
+type VideoProps = {
+    project: string
+}
 
 const Portfolio = () => {
+
+    const playVideo: React.FC<VideoProps> = ({ project }) => {
+        return (
+            <div className='video-container'>
+                <video width="640" height="360" controls>
+                    <source src={project} type="video/mp4" />
+                </video>
+            </div>
+        );
+    }
+
     return (
         <div className="portfolio-page">
             <div className="projects-grid">
@@ -12,13 +26,14 @@ const Portfolio = () => {
                     projects.map((proj, i) => (
                         <div className="project-card"
                             key={i}
+                            onClick={() => { playVideo({ project: proj }) }}
                         >
                             <img src={proj} alt="no image found" />
                         </div>
                     ))
                 }
             </div>
-        </div>
+        </div >
     );
 }
 
