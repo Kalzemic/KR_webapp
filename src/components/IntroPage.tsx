24df9f95b75aa2decd50b809e98733d371b19a92
kalzemic <Mizizov@gmail.com>
2026-02-04 02:04:38 +0200

    shadcn/ui+tailwind  integration

--- src/components/IntroPage.tsx
+++ src/components/IntroPage.tsx
@@ -1,12 +1,13 @@
 import { useNavigate } from 'react-router-dom'
 import '../styles/IntroPage.css'
 import { useTheme } from './ThemContext'
-
+import { Button } from "./ui/button"
 
 
 
 export default function IntroPage(){
 
+    
 
 
 
@@ -16,26 +17,27 @@ export default function IntroPage(){
     return(
         <div className='intro-page'>
 
+            <Button className='business-button' 
+                onClick={()=>{setTheme('business'); navigate('/display')}}>
+                לתוכן עסקים
+            </Button>
             <video id='business-video' className='intro-video' autoPlay loop muted playsInline
-            onLoadedData={(e) => e.currentTarget.playbackRate = 0.7}
+            onLoadedData={(e) => e.currentTarget.playbackRate = 0.99}
             >
                 <source  src="/intro/business.mp4" type="video/mp4" 
                 />
             </video>
-            <button className='business-button' 
-            onClick={()=>{setTheme('business'); navigate('/display')}}>
-                לתוכן עסקים
-            </button>
+            <Button className='events-button' 
+                onClick={()=>{setTheme('events'); navigate('/display')}}>
+                לתוכן אירועים
+            </Button>
 
             <video id='events-video' className='intro-video' autoPlay loop muted playsInline
             onClick={()=>{setTheme('events'); navigate('/display')}}
             >
                 <source src="/intro/events.mp4" type="video/mp4"/>
             </video>
-            <button className='events-button' 
-            onClick={()=>{setTheme('events'); navigate('/display')}}>
-            לתוכן אירועים
-            </button>
+
         </div>
     )
 }
